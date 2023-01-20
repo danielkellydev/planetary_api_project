f4fbb6a5e6d40b414ed4b3aa2c9905426806288c
danielkellydev <danielkellydev@gmail.com>
2023-01-20 13:34:51 +1100

    added routes: not_found, parameters, super_simple

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,23 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the Planetary API'
+    return jsonify(message='Hello from the Planetary API.'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message = "Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+         
+
 
 if __name__ == '__main__':
     app.run
